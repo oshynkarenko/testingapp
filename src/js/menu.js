fa507a3da39fc2899cc47a87535fc082ee6abf46
oshynkarenko <shynkarenko.o@gmail.com>
2018-11-29 15:33:18 +0200

    slider updated

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,13 +1,12 @@
 export default class Menu {
     constructor(){
-        this.openLink = document.getElementById('menu-burger');
         this.container = document.getElementById('header-menu');
-        if (this.openLink) {
-            this.openLink.addEventListener('click', this.render.bind(this));
-        }
+        document.addEventListener('click', this.render.bind(this));
     }
 
     render() {
-        this.container.classList.add('header__menu--mobile');
+        if (event.target.classList.contains('header__menu__button')) {
+            this.container.classList.add('header__menu--mobile');
+        }
     }
 }
